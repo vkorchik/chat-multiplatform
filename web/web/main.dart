5ee54d7d51e016173384946b2778cf0587d0204b
lukasz_huculak <lukasz.huculak@codete.com>
2018-07-28 09:09:32 +0200

    Create web Firestore message service

--- web/web/main.dart
+++ web/web/main.dart
@@ -3,8 +3,10 @@ import 'dart:html';
 import 'package:react/react_client.dart' as react_client;
 import 'package:react/react_dom.dart' as react_dom;
 import 'chat_app.dart';
+import 'firestore_init.dart';
 
 void main() {
+  initFirestore();
   react_client.setClientConfiguration();
   var chatApp = chatAppComponent({});
   react_dom.render(chatApp, querySelector('#react_mount_point'));
--- web/web/service_provider.dart
+++ web/web/service_provider.dart
@@ -1,5 +1,8 @@
 import 'package:chat_shared/services/message_service.dart';
 import 'package:chat_shared/services/echo_message_service.dart';
 
+import 'firestore_message_service.dart';
+
 var _echoService = new EchoMessageService();
-MessageService createMessageService() => _echoService;
+var _firestoreService = new FirestoreMessageService();
+MessageService createMessageService() => _firestoreService;
